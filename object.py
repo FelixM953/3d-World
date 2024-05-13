08ec520cd3b0a39cee9bb8d63802ca5201645902
FelixM953 <113046662+FelixM953@users.noreply.github.com>
2024-05-13 09:29:30 -0700

    Update object.py

--- object.py
+++ object.py
@@ -10,3 +10,15 @@ class Cube:
             (4, 5), (5, 6), (6, 7), (7, 4),
             (0, 4), (1, 5), (2, 6), (3, 7)
                  ]
+        
+class Pyramid:
+    def __init__(self, position, width, height):
+        self.origin = position
+        self.vertecies = [
+            (-width/2, height/2, -width/2), (width/2, height/2, -width/2), (width/2, height/2, width/2), (-width/2, height/2, width/2),
+            (0, -height/2, 0)
+        ]
+        self.edges = [
+            (0, 1), (1, 2), (2, 3), (3, 0),
+            (0, 4), (1, 4), (2, 4), (3, 4)
+        ]
