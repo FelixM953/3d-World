10f274cda752b8476c9714bd09c781bfee9da89d
FelixM953 <113046662+FelixM953@users.noreply.github.com>
2024-05-13 09:29:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import pygame
 import pygame.display
 from sys import exit
 import math
-from object import Cube
+from object import Cube, Pyramid
 
 FOCAL_LENGTH = 150
 
-pos = (0, 0, 15)
-c = Cube(pos, 5)
-objects = [c]
+
+c = Cube((-5, 0, 10), 5)
+p = Pyramid((5, 0, 10), 5, 5)
+objects = [c, p]
 
 WIDTH, HEIGHT = 500, 500
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -29,21 +30,25 @@ def apply_offset(origin, vertex):
     return (vertex[0]+origin[0], vertex[1]+origin[1], vertex[2]+origin[2])
 
 
-def draw_wireframes():
-    for object in objects:
-        for vertex in object.vertecies:
-            shifted_vertex = apply_offset(object.origin, vertex)
-            x, y = project_vertex(shifted_vertex, FOCAL_LENGTH)
-            pygame.draw.circle(screen, (255,255,255), (x+WIDTH//2, y+HEIGHT//2), 5)
-        for edge in object.edges:
-            v1, v2 = [object.vertecies[i] for i in edge]
-            shifted_v1 = apply_offset(object.origin, v1)
-            shifted_v2 = apply_offset(object.origin, v2)
-            x1, y1 = project_vertex(shifted_v1, FOCAL_LENGTH)
-            x2, y2 = project_vertex(shifted_v2, FOCAL_LENGTH)
+def draw_wireframe(object):
+    for vertex in object.vertecies:
+        shifted_vertex = apply_offset(object.origin, vertex) # shift the local coordinates of the vertecies into global cordinates
+        x, y = project_vertex(shifted_vertex, FOCAL_LENGTH)
+        pygame.draw.circle(screen, (255,255,255), (x+WIDTH//2, y+HEIGHT//2), 0.25*FOCAL_LENGTH//shifted_vertex[2])
+    for edge in object.edges:
+        v1, v2 = [object.vertecies[i] for i in edge]
+        shifted_v1 = apply_offset(object.origin, v1)
+        shifted_v2 = apply_offset(object.origin, v2)
+        x1, y1 = project_vertex(shifted_v1, FOCAL_LENGTH)
+        x2, y2 = project_vertex(shifted_v2, FOCAL_LENGTH)
 
-            pygame.draw.line(screen, (255, 255, 255), (x1+WIDTH//2, y1+HEIGHT//2), (x2+WIDTH//2, y2+HEIGHT//2), 1)
-            
+        pygame.draw.line(screen, (255, 255, 255), (x1+WIDTH//2, y1+HEIGHT//2), (x2+WIDTH//2, y2+HEIGHT//2), 1)
+
+
+def move_camera(shift):
+    for object in objects:
+        object.origin = (object.origin[0]+shift[0], object.origin[1]+shift[1], object.origin[2]+shift[2])
+
 
 while True:
     clock.tick(60)
@@ -54,17 +59,17 @@ while True:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        objects[0].origin = (objects[0].origin[0]+0.5, objects[0].origin[1], objects[0].origin[2])
+        move_camera((0.5, 0, 0))
     if keys[pygame.K_RIGHT]:
-        objects[0].origin = (objects[0].origin[0]-0.5, objects[0].origin[1], objects[0].origin[2])
+        move_camera((-0.5, 0, 0))
     if keys[pygame.K_UP] and FOCAL_LENGTH > 1:
-        objects[0].origin = (objects[0].origin[0], objects[0].origin[1]+0.5, objects[0].origin[2])
+        move_camera((0, 0.5, 0))
     if keys[pygame.K_DOWN]:
-        objects[0].origin = (objects[0].origin[0], objects[0].origin[1]-0.5, objects[0].origin[2])
+        move_camera((0, -0.5, 0))
     if keys[pygame.K_PAGEUP] and FOCAL_LENGTH > 1:
-        objects[0].origin = (objects[0].origin[0], objects[0].origin[1], objects[0].origin[2]-0.5)
+        move_camera((0, 0, 0.5))
     if keys[pygame.K_PAGEDOWN]:
-        objects[0].origin = (objects[0].origin[0], objects[0].origin[1], objects[0].origin[2]+0.5)
+        move_camera((0, 0, -0.5))
 
     if keys[pygame.K_LEFTBRACKET] and FOCAL_LENGTH > 1:
         FOCAL_LENGTH -= 5
@@ -74,6 +79,6 @@ while True:
 
 
     screen.fill((0, 0, 0))
-    draw_wireframes()
-    # draw_sides()
+    for object in objects:
+        draw_wireframe(object)
     pygame.display.update()
